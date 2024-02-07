52ff7f5e931e4676c0297e12c8b374104fdff6ef
andrenormanlang <andrenormanlang@gmail.com>
2024-02-07 16:40:06 +0100

    added responsive characters list

--- src/helpers/Superhero-API/CharactersReferenceListTable.tsx
+++ src/helpers/Superhero-API/CharactersReferenceListTable.tsx
@@ -1,68 +1,75 @@
 'use client'
 
-/* eslint-disable */
-// @ts-nocheck
-
-import { CharactersApiResponse, CharactersList } from '@/types/characters-list.types';
 import { useRouter } from 'next/navigation';
-import { Table, Thead, Tbody, Tr, Th, Td, Flex, useColorModeValue } from '@chakra-ui/react';
+import { useBreakpointValue, Table, Thead, Tbody, Tr, Th, Td, Flex, useColorModeValue } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 
+
+export type Character = {
+	ID: number;
+	CharacterName: string;
+  };
+
+
+  export type CharactersApiResponse = {
+	data: Character[];
+	status: string;
+  };
+
 const CharactersReferenceListTable: React.FC<{ characters:CharactersApiResponse }> = ({ characters }) => {
-  // Correctly initialize sortConfig with SortConfig type
+
  const router = useRouter();
  const bg = useColorModeValue('red.100', 'red.700');
- const columns = 3;
+ const columns = useBreakpointValue({ base: 1, md: 2, lg: 3 });
 
- const [characterColumns, setCharacterColumns] = useState<CharactersApiResponse[][]>([]);
+
+
+ const [characterColumns, setCharacterColumns] = useState<Character[][]>([]);
 
   const handleRowClick = (characterId: number) => {
     // Navigate to the SuperheroID page with the character ID
     router.push(`/search/superheros/superhero-api/${characterId}`);
   };
 
+useEffect(() => {
 
-
-  useEffect(() => {
-	// Access the array with characters.data.data
 	const charactersAny = characters as any;
 	if (Array.isArray(charactersAny.data.data)) {
-	  const columnSize = Math.ceil(charactersAny.data.data.length / columns);
+	  const columnSize = Math.ceil(charactersAny.data.data.length / (columns || 1));
 	  const newCharacterColumns = new Array(columns).fill(null).map((_, index) => {
 		return charactersAny.data.data.slice(index * columnSize, (index + 1) * columnSize);
 	  });
 	  setCharacterColumns(newCharacterColumns);
 	} else {
-	  console.log('Data is not an array:', charactersAny.data);
+	  console.log('Data is not an array:', characters.data);
 	}
-  }, [characters]);
-
+  }, [characters, columns]);
 
   return (
-    <Flex direction="row" overflowX="auto" h="80vh">
-      {characterColumns.map((columnCharacters, index) => (
-        <Table key={index} variant="simple" size="sm" mx={4}>
-          <Thead>
-            <Tr>
-              <Th>ID</Th>
-              <Th>Character Name</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-		  {columnCharacters.map((character: CharactersApiResponse)  => (
-              <Tr
+	<Flex direction="row" overflowX="auto" h="80vh">
+	{characterColumns.map((column, columnIndex) => (
+	  <Table key={columnIndex} variant="simple" size="sm" mx={4}>
+		<Thead>
+		  <Tr>
+			<Th>ID</Th>
+			<Th>Character Name</Th>
+		  </Tr>
+		</Thead>
+		<Tbody>
+		  {column.map((character) => ( 
+			<Tr
 			  key={character.ID}
 			  _hover={{ bg: bg, cursor: 'pointer' }}
 			  onClick={() => handleRowClick(character.ID)}
 			>
-                <Td>{character.ID}</Td>
-                <Td>{character.CharacterName}</Td>
-              </Tr>
-            ))}
-          </Tbody>
-        </Table>
-      ))}
-    </Flex>
+			  <Td>{character.ID}</Td>
+			  <Td>{character.CharacterName}</Td>
+			</Tr>
+		  ))}
+		</Tbody>
+	  </Table>
+	))}
+  </Flex>
   );
 };
 
--- src/hooks/superhero-api/useGetCharactersList.ts
+++ src/hooks/superhero-api/useGetCharactersList.ts
@@ -1,44 +1,3 @@
-'use client'
-
-// import { CharactersList } from '@/types/characters-list.types';
-// import { useState, useEffect } from 'react';
-
-
-// const useGetCharactersList = () => {
-//   const [characters, setCharacters] = useState<CharactersList[]>([]);
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [error, setError] = useState<Error | null>(null);
-
-//   useEffect(() => {
-//     const fetchCharacters = async () => {
-//       setIsLoading(true);
-//       try {
-//         // Replace '/api/characters' with your API route if different
-//         const response = await fetch('/api/characters-list');
-//         if (!response.ok) {
-//           throw new Error(`API call failed with status: ${response.status}`);
-//         }
-//         const data = await response.json();
-//         setCharacters(data.data); // Assuming the response has a 'data' field with the characters array
-//       } catch (e) {
-//         if (e instanceof Error) {
-//           setError(e);
-//         } else {
-//           setError(new Error('An unexpected error occurred'));
-//         }
-//       } finally {
-//         setIsLoading(false);
-//       }
-//     };
-
-//     fetchCharacters();
-//   }, []); // The empty array ensures this effect runs only once when the component mounts
-
-//   return { characters, isLoading, error };
-// };
-
-// export default useGetCharactersList;
-
 import { useQuery } from '@tanstack/react-query';
 import { CharactersList } from '@/types/characters-list.types';
 
@@ -54,8 +13,6 @@ const useGetCharactersList = () => {
   return useQuery<CharactersList[], Error>({
     queryKey: ['charactersList'],
     queryFn: fetchCharactersList,
-    // Optionally, you can configure how React Query handles this data,
-    // such as caching, refetching, stale time, etc.
   });
 };
 

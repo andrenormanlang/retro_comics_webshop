539fff4ff18486baaf6ad5cc341cc572195dea00
andrenormanlang <andrenormanlang@gmail.com>
2024-02-11 23:55:12 +0100

    fecthing all images for comic-vine description!

--- src/helpers/ComicVineIssues/ComicVineCharacterDescription.tsx
+++ src/helpers/ComicVineIssues/ComicVineCharacterDescription.tsx
@@ -21,19 +21,19 @@ interface DomNode {
 const ComicVineCharacterDescription: React.FC<ComicVineCharacterDescriptionProps> = ({ content, scrollPosition }) => {
 	const options = {
 		replace: (domNode: DomNode) => {
-			if (domNode.name === "img") {
+			if (domNode.name === "img" && domNode.attribs["data-src"]) {
 				const attribs = domNode.attribs as { [key: string]: string; src: string; alt: string };
 				return (
 					<LazyLoadImage
-						alt={attribs.alt}
-						height="auto"
-						src={attribs.src}
-
-						effect="blur"
-						placeholderSrc={attribs["data-placeholder"] ?? "path_to_your_placeholder_image"}
-						scrollPosition={scrollPosition}
-						width="300px" // Set a fixed width
-						style={{ maxWidth: '100%', height: 'auto' }}
+					alt={attribs.alt}
+					height="auto"
+					src={attribs["data-src"]}
+					effect="blur"
+					placeholderSrc={attribs["data-placeholder"] ?? "path_to_your_placeholder_image"}
+					scrollPosition={scrollPosition}
+					width="300px"
+					style={{ maxWidth: '100%', height: 'auto' }}
+					srcSet={attribs["data-srcset"]}
 
 					/>
 				);

c7e0f975497b63d01edf7b241e3699d06c23e88c
andrenormanlang <andrernormanlang@gmail.com>
2024-06-20 10:53:41 +0200

    cleaning up code on callback

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -15,27 +15,6 @@ export async function GET(request: Request) {
   }
 
   // URL to redirect to after sign in process completes
-//   Based on sign up process, redirect to account page
-
   return NextResponse.redirect(`${requestUrl.origin}/auth/account`);
 }
 
-
-// import { createClient } from "@/utils/supabase/server";
-// import { NextResponse } from "next/server";
-
-// export async function GET(request: Request) {
-//   // The `/auth/callback` route is required for the server-side auth flow implemented
-//   // by the SSR package. It exchanges an auth code for the user's session.
-//   // https://supabase.com/docs/guides/auth/server-side/nextjs
-//   const requestUrl = new URL(request.url);
-//   const code = requestUrl.searchParams.get("code");
-
-//   if (code) {
-//     const supabase = createClient();
-//     await supabase.auth.exchangeCodeForSession(code);
-//   }
-
-//   // URL to redirect to after sign in process completes
-//   return NextResponse.redirect(requestUrl.origin);
-// }
